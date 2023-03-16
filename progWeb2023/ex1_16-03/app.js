78047c75129f74c7a7d0415d852822b7a0b94b67
Leonardo Terra <84455182+terraleonardo@users.noreply.github.com>
2023-03-15 23:34:09 -0300

    Add files via upload

--- progWeb2023/ex1_16-03/app.js
+++ progWeb2023/ex1_16-03/app.js
@@ -1,11 +1,14 @@
+//Leonardo Terra dos Santos - 401 Info
 const casual = require("casual").pt_BR;
 
-//criar função para CEP aqui!! (teste de branch do github também)
+casual.define('cep', function() {
+    return Math.random().toString().slice(2,10);
+});
 
 for(var i = 0; i < 10; i++) {
     console.log("Nome e sobrenome: "+casual.full_name+" "+casual.last_name);
     console.log("Rua: "+casual.street);
     console.log("Cidade: "+casual.city);
     console.log("País: "+casual.country);
-    console.log("CEP: "/*inserir função CEP aqui*/+"\n");
+    console.log("CEP: "+casual.cep+"\n");
 }
