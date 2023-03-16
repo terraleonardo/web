6d8cca7f223f6c9469c49ac239c5db456699be6e
Leonardo Terra <84455182+terraleonardo@users.noreply.github.com>
2023-03-15 23:39:11 -0300

    Update app.js

--- progWeb2023/ex1_16-03/app.js
+++ progWeb2023/ex1_16-03/app.js
@@ -1,11 +1,14 @@
+//Leonardo Terra dos Santos - 401 Info
 const casual = require("casual").pt_BR;
 
-
+casual.define('cep', function() {
+    return Math.random().toString().slice(2,10);
+});
 
 for(var i = 0; i < 10; i++) {
     console.log("Nome e sobrenome: "+casual.full_name+" "+casual.last_name);
     console.log("Rua: "+casual.street);
     console.log("Cidade: "+casual.city);
     console.log("País: "+casual.country);
-    console.log("CEP: "+casual.log+"\n");
+    console.log("CEP: "+casual.cep+"\n");
 }
