6b74e1b60d98aa44841051981278a1c98b17a9d9
Leonardo Terra <84455182+terraleonardo@users.noreply.github.com>
2023-03-31 16:31:51 -0300

    Update calculao.js

--- mathListGenerator/calculao.js
+++ mathListGenerator/calculao.js
@@ -1,25 +1,25 @@
-function geraMilDivisoes(dividendoMin, divisorMin) {
-    const lista = [];
+function geraMilDivisoes(dividendoMin, divisorMin) { //gera mil possibilidades de divisões a partir de um dividendo mínimo e um divisor mínimo.
+    const lista = []; //cria array da lista de divisões
 
-    for(let i = dividendoMin; i <= (dividendoMin + 1000); i++) {
-        for(let j = divisorMin; j <= (divisorMin + 1000); j++) {
-            if(i != j && (i % j) == 0) {
-                lista.push(i+" / "+j);
+    for(let i = dividendoMin; i <= (dividendoMin + 1000); i++) { //loop passando dividendo
+        for(let j = divisorMin; j <= (divisorMin + 1000); j++) { //loop passando divisores para cada dividendo
+            if(i != j && (i % j) == 0) { //confere se a divisão é exata
+                lista.push(i+" / "+j); //se for exata, "empurra" pra dentro do array de divisões
             }
         }
     }
 
-    return lista;
+    return lista; //retorna lista com mil divisões exatas
 }
 
-function geraLista(listaInteira, qtdExercicios) {
-    let listaCropada = [];
+function geraLista(listaMil, qtdExercicios) { //recebe por parâmetro o array de mil, depois a quantidade de exercícios que quer gerar na lista
+    let listaCropada = []; //lista para inserir as divisões aleatórias
     
-    for(let i = 0; i < qtdExercicios; i++) {
-        listaCropada.push(listaInteira[Math.floor(Math.random() * 1001)]);
+    for(let i = 0; i < qtdExercicios; i++) { //loop para rodar a quantidade de exercícios passados por parâmetro
+        listaCropada.push(listaMil[Math.floor(Math.random() * 1001)]); //insere uma divisão aleatória da lista de mil na lista cropada
     }
 
-    return listaCropada;
+    return listaCropada; //retorna lista com x divisões aleatórias da lista de mil
 }
 
 console.log(geraLista(geraMilDivisoes(100, 20), 10));
@@ -27,7 +27,7 @@ console.log(geraLista(geraMilDivisoes(100, 20), 10));
 function makeList() {
     let listData = geraLista(geraMilDivisoes(100, 20), 10);
 
-    let listContainer = document.createElement('div');
+    let listContainer = document.querySelector('.container-lista');
 
     let listElement = document.createElement('ol');
 
@@ -38,7 +38,7 @@ function makeList() {
     
     let numberOfListItems = listData.length;
 
-    for (let i = 0; i < numberOfListItems; ++i) {
+    for (let i = 0; i < numberOfListItems; i++) {
         listItem.textContent = listData[i];
         listElement.appendChild(listItem);
 
